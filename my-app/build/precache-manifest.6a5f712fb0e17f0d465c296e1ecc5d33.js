a448d6dd734b733211487b1f1778c631f0548d65
dan870627 <45359891+dan870627@users.noreply.github.com>
2019-04-25 20:26:03 +0800

    my-app/build
    add shopWeb

---
+++ my-app/build/precache-manifest.6a5f712fb0e17f0d465c296e1ecc5d33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "da3bbd581bf6a30bce33",
+    "url": "/static/js/main.109a79ac.chunk.js"
+  },
+  {
+    "revision": "131359c7dfb15956220b",
+    "url": "/static/js/2.4071c3b1.chunk.js"
+  },
+  {
+    "revision": "da3bbd581bf6a30bce33",
+    "url": "/static/css/main.3b6ef321.chunk.css"
+  },
+  {
+    "revision": "131359c7dfb15956220b",
+    "url": "/static/css/2.8b5cd746.chunk.css"
+  },
+  {
+    "revision": "3e92a6f0589489a1948ab8a9b8aa2781",
+    "url": "/index.html"
+  }
+];
