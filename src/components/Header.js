a9eff97887c3f96ff2871b6efd0f99834a562504
ChiaAn <dan870627@gmail.com>
2019-04-26 20:00:07 +0800

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ class Header extends Component {
       <div className="App">
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
           <div className="container">
-            <Link className="navbar-brand" to="/">Start Bootstrap</Link>
+            <Link className="navbar-brand" to="/react-shopweb/">Start Bootstrap</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
