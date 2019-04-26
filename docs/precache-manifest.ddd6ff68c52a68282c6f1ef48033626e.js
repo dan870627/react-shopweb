f0b557669bfd274ab1413d02a094765380b7d33f
ChiaAn <dan870627@gmail.com>
2019-04-26 22:14:18 +0800

    updata docs

---
+++ docs/precache-manifest.ddd6ff68c52a68282c6f1ef48033626e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "b51d3e03c5e0134b29e4",
+    "url": "./static/js/main.1c640074.chunk.js"
+  },
+  {
+    "revision": "ad91f7f81a2375da8cf6",
+    "url": "./static/js/2.4071c3b1.chunk.js"
+  },
+  {
+    "revision": "b51d3e03c5e0134b29e4",
+    "url": "./static/css/main.bc127e99.chunk.css"
+  },
+  {
+    "revision": "ad91f7f81a2375da8cf6",
+    "url": "./static/css/2.39a94384.chunk.css"
+  },
+  {
+    "revision": "0eedf370d4b736b1c7c7f3dc5c4860db",
+    "url": "./index.html"
+  }
+];
